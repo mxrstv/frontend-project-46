d50159319b813d43a932a256fae2378cea63e2c5
Max Erastov <max.erastov@gmail.com>
2023-02-16 00:51:50 +0300

    Add options

--- gendiff.js
+++ gendiff.js
@@ -4,4 +4,6 @@ import { program } from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv);
