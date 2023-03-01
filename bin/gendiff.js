5e302cf6c5a5155f63369f5da0964afd9e45d385
Max Erastov <max.erastov@gmail.com>
2023-03-01 14:06:32 +0300

    Get diff lines in array

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,12 +8,36 @@ const normalizePath = (str) => {
   return path.isAbsolute(str) ? str : path.resolve(cwd, str);
 };
 
-const getObjects = (filepath1, filepath2, format) => {
+const compareObjects = (filepath1, filepath2, format) => {
   const [first, second] = [filepath1, filepath2]
     .map((str) => normalizePath(str))
     .map((filePath) => fs.readFileSync(filePath, { encoding: 'utf8' }))
     .map((strObj) => JSON.parse(strObj));
-  console.log(first, second);
+  const keys = [first, second]
+    .flatMap((obj) => Object.keys(obj))
+    .filter((item, index, items) => items.indexOf(item) === index)
+    .sort();
+  // console.log(first, second);
+  console.log(keys);
+  const lines = [];
+  keys.forEach((key) => {
+    console.log(`${key} first: ${first[key]} second: ${first[key]} `);
+    if (Object.hasOwn(first, key) && Object.hasOwn(second, key)) {
+      if (first[key] === second[key]) {
+        lines.push(`  ${key}: ${first[key]}`);
+      } else {
+        lines.push(`- ${key}: ${first[key]}`);
+        lines.push(`+ ${key}: ${second[key]}`);
+      }
+    }
+    if (Object.hasOwn(first, key) && !Object.hasOwn(second, key)) {
+      lines.push(`- ${key}: ${first[key]}`);
+    }
+    if (!Object.hasOwn(first, key) && Object.hasOwn(second, key)) {
+      lines.push(`+ ${key}: ${second[key]}`);
+    }
+  });
+  console.log(lines);
 };
 
 program
@@ -21,8 +45,9 @@ program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
+  // .helpOption('-h, --help', 'output usage information')
   .action((filepath1, filepath2) => {
     const options = program.opts();
-    getObjects(filepath1, filepath2, options.format);
+    compareObjects(filepath1, filepath2, options.format);
   })
   .parse(process.argv);
