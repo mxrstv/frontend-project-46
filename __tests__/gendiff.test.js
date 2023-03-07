3b5164692bbc97dd1c1e8cd45393f1773d288041
Max Erastov <max.erastov@gmail.com>
2023-03-07 19:15:01 +0300

    Add tests for plain yaml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,9 +7,17 @@ import gendiff from '../src/index.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('plain object', () => {
+test('plain object json', () => {
   const firstPlain = getFixturePath('first_plain.json');
   const secondPlain = getFixturePath('second_plain.json');
+  const expected = fs.readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
+  const actual = gendiff(firstPlain, secondPlain);
+  expect(actual).toEqual(expected);
+});
+
+test('plain object yaml', () => {
+  const firstPlain = getFixturePath('first_plain.yml');
+  const secondPlain = getFixturePath('second_plain.yml');
   const expected = fs.readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
   const actual = gendiff(firstPlain, secondPlain);
   expect(actual).toEqual(expected);
