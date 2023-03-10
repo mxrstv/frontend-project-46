9254f52e318bb5a12bb5d49b71cceed962c6ab69
Max Erastov <max.erastov@gmail.com>
2023-03-10 21:43:51 +0300

    Add tests for nested objects

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,18 +7,18 @@ import gendiff from '../src/index.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('plain object json', () => {
-  const firstPlain = getFixturePath('first_plain.json');
-  const secondPlain = getFixturePath('second_plain.json');
-  const expected = fs.readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
+test('json', () => {
+  const firstPlain = getFixturePath('file1.json');
+  const secondPlain = getFixturePath('file2.json');
+  const expected = fs.readFileSync(getFixturePath('expected.txt'), 'utf-8');
   const actual = gendiff(firstPlain, secondPlain);
   expect(actual).toEqual(expected);
 });
 
-test('plain object yaml', () => {
-  const firstPlain = getFixturePath('first_plain.yml');
-  const secondPlain = getFixturePath('second_plain.yml');
-  const expected = fs.readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
+test('yaml', () => {
+  const firstPlain = getFixturePath('file1.yml');
+  const secondPlain = getFixturePath('file2.yml');
+  const expected = fs.readFileSync(getFixturePath('expected.txt'), 'utf-8');
   const actual = gendiff(firstPlain, secondPlain);
   expect(actual).toEqual(expected);
 });
