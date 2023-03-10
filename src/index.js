023cb9aa61f42ab61def358fcb6fc68fb5294b1c
Max Erastov <max.erastov@gmail.com>
2023-03-11 01:33:07 +0300

    Move file reading from parsers to gendiff

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+import _ from 'lodash';
+import path from 'node:path';
+import * as fs from 'node:fs';
 import parseObj from './parsers.js';
 
+const normalizePath = (str) => {
+  const cwd = process.cwd();
+  return path.isAbsolute(str) ? str : path.resolve(cwd, str);
+};
+
 const gendiff = (filepath1, filepath2, format) => {
+  // const ext = path.extname(filepath);
+  // const normalized = normalizePath(filepath);
+  // const strObj = fs.readFileSync(normalized, { encoding: 'utf8' });
   const [first, second] = [filepath1, filepath2]
-    .map((file) => parseObj(file));
-  const keys = [first, second]
-    .flatMap((obj) => Object.keys(obj))
-    .filter((item, index, items) => items.indexOf(item) === index)
-    .sort();
-  // console.log(keys);
+    .map((filepath) => normalizePath(filepath))
+    .map((normPath) => [fs.readFileSync(normPath, { encoding: 'utf8' }), path.extname(normPath)])
+    .map(([strObj, ext]) => parseObj(strObj, ext));
+  console.log(first, second);
+  const keys = _.sortBy(_.union(_.keys(first), _.keys(second)));
+  console.log(keys);
   const lines = [];
   keys.forEach((key) => {
     // console.log(`${key} first: ${first[key]} second: ${first[key]} `);
--- src/parsers.js
+++ src/parsers.js
@@ -1,24 +1,12 @@
-import path from 'node:path';
-import * as fs from 'node:fs';
 import yaml from 'js-yaml';
 
-const normalizePath = (str) => {
-  const cwd = process.cwd();
-  return path.isAbsolute(str) ? str : path.resolve(cwd, str);
-};
-
-const parseObj = (filepath) => {
-  const ext = path.extname(filepath);
-  //  console.log(`ext: ${ext}`);
-  const parsers = {
-    '.json': (strObj) => JSON.parse(strObj),
-    '.yml': (strObj) => yaml.load(strObj),
-    '.yaml': (strObj) => yaml.load(strObj),
+const parseObj = (strObj, ext) => {
+  const formats = {
+    '.json': (format) => JSON.parse(format),
+    '.yml': (format) => yaml.load(format),
+    '.yaml': (format) => yaml.load(format),
   };
-  const normalized = normalizePath(filepath);
-  const strObj = fs.readFileSync(normalized, { encoding: 'utf8' });
-  //  console.log(`strObj: ${strObj}`);
-  return parsers[ext]?.(strObj) ?? `Format: ${ext} not supported`;
+  return formats[ext]?.(strObj) ?? `Format: ${ext} not supported`;
 };
 
 export default parseObj;
