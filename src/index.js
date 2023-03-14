87583efe9f2cef053bef37855aef7e21deef5dea
Max Erastov <max.erastov@gmail.com>
2023-03-14 14:25:32 +0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -4,41 +4,40 @@ import * as fs from 'node:fs';
 import parseObj from './parsers.js';
 import formatDiff from './stylish.js';
 
-const normalizePath = (str) => {
-  const cwd = process.cwd();
-  return path.isAbsolute(str) ? str : path.resolve(cwd, str);
+const normalizePath = (str) => path.resolve(process.cwd(), str);
+const readFile = (filePath) => fs.readFileSync(filePath, { encoding: 'utf8' });
+
+const buildDiffTree = (first, second) => {
+  const keys = _.sortBy(_.union(_.keys(first), _.keys(second)));
+  const diffTree = keys.map((key) => {
+    if (!_.has(second, key)) {
+      return { name: [key], state: 'deleted', value: first[key] };
+    }
+    if (!_.has(first, key)) {
+      return { name: [key], state: 'added', value: second[key] };
+    }
+    if (_.isPlainObject(first[key]) && _.isPlainObject(second[key])) {
+      return { name: [key], state: 'nested', value: buildDiffTree(first[key], second[key]) };
+    }
+    return (_.isEqual(first[key], second[key]))
+      ? { name: [key], state: 'unchanged', value: first[key] }
+      : {
+        name: [key],
+        state: 'changed',
+        value: second[key],
+        oldValue: first[key],
+      };
+  });
+  return diffTree;
 };
 
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
-  const [firstObject, secondObject] = [filepath1, filepath2]
+  const objects = [filepath1, filepath2]
     .map((filepath) => normalizePath(filepath))
-    .map((normPath) => [fs.readFileSync(normPath, { encoding: 'utf8' }), path.extname(normPath)])
+    .map((normPath) => [readFile(normPath), path.extname(normPath)])
     .map(([strObj, ext]) => parseObj(strObj, ext));
-  // console.log(firstObject, secondObject);
-  const buildDiffTree = (first, second) => {
-    const keys = _.sortBy(_.union(_.keys(first), _.keys(second)));
-    const diffTree = keys.map((key) => {
-      if (!_.has(second, key)) {
-        return { name: [key], state: 'deleted', value: first[key] };
-      }
-      if (!_.has(first, key)) {
-        return { name: [key], state: 'added', value: second[key] };
-      }
-      if (_.isPlainObject(first[key]) && _.isPlainObject(second[key])) {
-        return { name: [key], state: 'nested', value: buildDiffTree(first[key], second[key]) };
-      }
-      return (_.isEqual(first[key], second[key]))
-        ? { name: [key], state: 'unchanged', value: first[key] }
-        : {
-          name: [key],
-          state: 'changed',
-          value: second[key],
-          oldValue: first[key],
-        };
-    });
-    return diffTree;
-  };
-  return formatDiff(buildDiffTree(firstObject, secondObject));
+  const [firstObj, secondObj] = objects;
+  return formatDiff(buildDiffTree(firstObj, secondObj));
 };
 
 export default gendiff;
