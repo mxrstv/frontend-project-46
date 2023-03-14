7ac4c2e9a21f6622d2e1720963a7b2961c6ff81e
Max Erastov <max.erastov@gmail.com>
2023-03-14 16:52:40 +0300

    Add tests for plain format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,18 +7,34 @@ import gendiff from '../src/index.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('json', () => {
-  const firstPlain = getFixturePath('file1.json');
-  const secondPlain = getFixturePath('file2.json');
-  const expected = fs.readFileSync(getFixturePath('expected.txt'), 'utf-8');
-  const actual = gendiff(firstPlain, secondPlain);
+test('json stylish', () => {
+  const first = getFixturePath('file1.json');
+  const second = getFixturePath('file2.json');
+  const expected = fs.readFileSync(getFixturePath('expected_stylish.txt'), 'utf-8');
+  const actual = gendiff(first, second);
   expect(actual).toEqual(expected);
 });
 
-test('yaml', () => {
-  const firstPlain = getFixturePath('file1.yml');
-  const secondPlain = getFixturePath('file2.yml');
-  const expected = fs.readFileSync(getFixturePath('expected.txt'), 'utf-8');
-  const actual = gendiff(firstPlain, secondPlain);
+test('yaml stylish', () => {
+  const first = getFixturePath('file1.yml');
+  const second = getFixturePath('file2.yml');
+  const expected = fs.readFileSync(getFixturePath('expected_stylish.txt'), 'utf-8');
+  const actual = gendiff(first, second);
+  expect(actual).toEqual(expected);
+});
+
+test('json plain', () => {
+  const first = getFixturePath('file1.json');
+  const second = getFixturePath('file2.json');
+  const expected = fs.readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
+  const actual = gendiff(first, second);
+  expect(actual).toEqual(expected);
+});
+
+test('yaml plain', () => {
+  const first = getFixturePath('file1.yml');
+  const second = getFixturePath('file2.yml');
+  const expected = fs.readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
+  const actual = gendiff(first, second);
   expect(actual).toEqual(expected);
 });
